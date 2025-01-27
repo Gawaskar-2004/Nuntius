a5e9c434aa5122627b37805ba36263cb9492521c
Gawaskar-2004 <gawaskard2004@gmail.com>
2025-01-27 18:32:46 +0530

    Add Chat and Group models

--- Nuntius/src/main/java/com/Nuntius/Nuntius/model/Chat.java
+++ Nuntius/src/main/java/com/Nuntius/Nuntius/model/Chat.java
@@ -1,4 +1,23 @@
 package com.Nuntius.Nuntius.model;
 
-public class chat {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="chat")
+public class Chat {
+    @Id
+    @Column(name="CHAT_ID")
+    private String chat_id;
+    @Column(name="CHAT_NAME")
+    private String chat_name;
+    @Column(name="IS_GROUP")
+    private Boolean is_group;
+    @Column(name="CREATED_AT")
+    private LocalDateTime created_at;
+
 }
--- Nuntius/src/main/java/com/Nuntius/Nuntius/model/group.java
+++ Nuntius/src/main/java/com/Nuntius/Nuntius/model/group.java
@@ -1,4 +1,25 @@
 package com.Nuntius.Nuntius.model;
 
-public class Group {
+import jakarta.persistence.*;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "group.")
+public class group {
+    @Id
+    @Column(name = "GROUP_ID")
+    private String group_id;
+
+    @Column(name = "GROUP_NAME")
+    private String group_name;
+
+    @Column(name = "CREATED_BY")
+    private String created_by;
+    @ManyToOne
+    @JoinColumn(name = "CREATED_BY", referencedColumnName = "USER_ID")
+    private LocalDateTime created_at;
+
+
 }
