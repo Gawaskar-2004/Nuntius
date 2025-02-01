e211ba587ecc284e55bf0d49ea8a21c5983b079d
Gawaskar-2004 <gawaskard2004@gmail.com>
2025-02-01 15:30:26 +0530

    Delete src directory

--- src/main/java/com/Nuntius/Nuntius/repository/AuthenticationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Nuntius.Nuntius.repository;
-
-import com.Nuntius.Nuntius.model.Authentication;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthenticationRepository extends JpaRepository<Authentication, Long> {
-}
